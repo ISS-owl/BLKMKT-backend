d29f655f6ab47a33c27838045d83a561abdcd898
saber <414085716@qq.com>
2020-12-02 22:13:23 +0800

    captcha module added

---
+++ blkmkt-captcha/src/main/java/io/github/blkmkt/captcha/BlkmktCaptchaApplication.java
@@ -0,0 +1,15 @@
+package io.github.blkmkt.captcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlkmktCaptchaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BlkmktCaptchaApplication.class, args);
+    }
+
+}
---
+++ blkmkt-captcha/src/test/java/io/github/blkmkt/captcha/BlkmktCaptchaApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.blkmkt.captcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlkmktCaptchaApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
