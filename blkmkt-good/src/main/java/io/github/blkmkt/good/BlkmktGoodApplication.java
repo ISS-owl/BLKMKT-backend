a1970caee0d0b51f42b051d94d64b4cbd40ec6cc
Sh-Zh-7 <2431297348@qq.com>
2020-12-05 14:42:35 +0800

    feat: add nacos service discovery.

--- blkmkt-good/src/main/java/io/github/blkmkt/good/BlkmktGoodApplication.java
+++ blkmkt-good/src/main/java/io/github/blkmkt/good/BlkmktGoodApplication.java
@@ -2,8 +2,10 @@ package io.github.blkmkt.good;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlkmktGoodApplication {
 
     public static void main(String[] args) {
