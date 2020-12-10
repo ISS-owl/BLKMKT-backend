604263204974f67cd6a7b5d61edcde01d688462a
Sh-Zh-7 <2431297348@qq.com>
2020-12-10 15:15:38 +0800

    fix: add Response class with no argument consturctor.

---
+++ blkmkt-auth-server/src/main/java/io/github/issowl/authserver/utils/JWTUtils.java
@@ -0,0 +1,4 @@
+package io.github.issowl.authserver.utils;
+
+public class JWTUtils {
+}
--- blkmkt-common/src/main/java/io/github/common/entity/Response.java
+++ blkmkt-common/src/main/java/io/github/common/entity/Response.java
@@ -3,12 +3,14 @@ package io.github.common.entity;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Response implements Serializable {
     @ApiModelProperty(name = "code", example = "200")
