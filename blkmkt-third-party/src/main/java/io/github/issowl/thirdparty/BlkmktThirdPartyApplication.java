a43475a1daa8b45ae6d1513c3398c4351e597079
Sh-Zh-7 <2431297348@qq.com>
2020-12-14 10:08:23 +0800

    fix: exclude the redis auto configuration class when starting the spring boot.

--- blkmkt-third-party/src/main/java/io/github/issowl/thirdparty/BlkmktThirdPartyApplication.java
+++ blkmkt-third-party/src/main/java/io/github/issowl/thirdparty/BlkmktThirdPartyApplication.java
@@ -2,10 +2,13 @@ package io.github.issowl.thirdparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
+@SpringBootApplication(exclude = { RedisAutoConfiguration.class })
 public class BlkmktThirdPartyApplication {
 
     public static void main(String[] args) {
