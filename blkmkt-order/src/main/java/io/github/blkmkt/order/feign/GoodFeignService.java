fa840fa74f922841d32d6b8e2c63b32bb39a5e40
Sh-Zh-7 <2431297348@qq.com>
2020-12-24 11:31:15 +0800

    fix: try to rename good module from blkmkt-product to blkmkt-good.

--- blkmkt-order/src/main/java/io/github/blkmkt/order/feign/GoodFeignService.java
+++ blkmkt-order/src/main/java/io/github/blkmkt/order/feign/GoodFeignService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "blkmkt-product")
+@FeignClient(value = "blkmkt-good")
 public interface GoodFeignService {
     @GetMapping("good/good/{id}")
     R info(@PathVariable("id") Integer id);
